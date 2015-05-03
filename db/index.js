47e9cd6fbd0163f9361b6f3cff59a30b34c19c79
Travis <travis.libby@xero.com>
2015-05-03 14:18:19 -0700

    Add ability to create user.

--- db/index.js
+++ db/index.js
@@ -2,4 +2,10 @@ var mongoose = require('mongoose');
 
 var connection = mongoose.createConnection('mongodb://whiskyadmin:scotchyscotch@ds031962.mongolab.com:31962/whisky-logger', 3000);
 
+// models
+var userSchema = require('./schemas/user');
+
+// define models on the connection object
+connection.model('User', userSchema);
+
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+// db connection and models
+var conn = require('./db');
+var User = conn.model('User');
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
+// POST - create a user
+app.post('/api/user', function(req, res) {
+  User.create(req.body, function(err) {
+    if (err) {
+      if (err.code = 11000) {
+        return res.sendStatus(409); // conflict
+      }
+    } else {
+      return res.sendStatus(500); // server error
+    }
+    res.sendStatus(200); // ok - user created
+  });
+});
+
 app.listen(3000);
-console.log('Listening...')
+console.log('Listening...');
