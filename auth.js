9a811b64af3bba48e98d7912d77581b90a371d3e
Travis <travis.libby@xero.com>
2015-05-07 09:51:29 -0700

    Add auth file.

---
+++ auth.js
@@ -0,0 +1,34 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// db connection and models
+var conn = require('./db');
+var User = conn.model('User');
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+module.exports = passport;
+
